c20fb880d33843fd82cc0400c3678543bf0ff7d6
VCruzSant <sviniantiago@gmail.com>
2024-06-09 15:03:12 -0300

    Agregation

---
+++ agregation.sql
@@ -0,0 +1,7 @@
+SELECT max(salary) as max_salary,
+min(salary) as min_salary,
+avg(salary) as avg_salary,
+sum(salary) as sum_salary,
+count(salary) as count_salary
+from users
+where first_name = 'Vini';
